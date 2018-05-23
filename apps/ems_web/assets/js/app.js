be1c1287a5cbe3282bd04ef347d16a8c426f2057
Ken Tan <ken@siliconjungles.com>
2018-05-24 02:00:24 +0800

    added react

--- apps/ems_web/assets/js/app.js
+++ apps/ems_web/assets/js/app.js
@@ -1,23 +1,13 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
+const App = () => (
+  <div>
+    <h1>Hello, world!</h1>
+  </div>
+)
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-// import socket from "./socket"
-
-// import "antd/dist/antd.min.css"
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
